8a4de4a048d70760faa75e5bcc2988e017488f79
sanchitaggarwal <sanchitagarwal108@gmail.com>
2014-05-27 05:33:05 +0530

    Fixed Participant details submission module

--- public/js/participant_details.js
+++ public/js/participant_details.js
@@ -1,13 +1,12 @@
-var FORM_PD,PHASENO,USERID,PARTICIPANT_TYPE,FIRSTNAME,LASTNAME;
+var FORM_PD,PHASENO,USERID,PARTICIPANT_TYPE,FIRSTNAME,LASTNAME,GROUPID;
 var AGE,EDUCATION,MODEOFCOMM,GENDER,MUSICAL_TRAINING,MUSIC_KIND,HEARING_PROBLEM,KEYBOARD_FAMILIARITY;
-var ParticipantDetails = [], TimeStamp;
-
+var ParticipantDetails = [], PD = [], TimeStamp;
 
 // For inserting dummy data in participant details form
 function DummyData(){
 	FORM_PD = document.ParticipantDetail;
   if(document.getElementById("Dummy").checked){
-    FORM_PD.USERID.value = 'Dummy_User_1234';
+    FORM_PD.USERID.value = 'NewParticipant';
 	  FORM_PD.FIRSTNAME.value = 'Dummy';
 	  FORM_PD.LASTNAME.value = 'User';
     FORM_PD.AGE.value = '25';
@@ -20,6 +19,7 @@ function DummyData(){
     FORM_PD.HEARING_PROBLEM.value = 'No';
     FORM_PD.KEYBOARD_FAMILIARITY.value = 'Yes';
     FORM_PD.PHASENO.value = 'Phase_1';
+	  FORM_PD.GROUPID.value = 'SVEF';
 	  document.getElementById("Consent").checked = true;
     document.getElementById("ExperimentMode").value = 'Visual_Error_FeedBack_Training';
     document.getElementById("NoOfMaps").value = '10';
@@ -29,7 +29,6 @@ function DummyData(){
     document.getElementById("Familirization").disabled = true;
     document.getElementById("InterStimulusInterval").value = '';
     document.getElementById("Familirization").checked = false;
-    disablePD();
 	}
 	else{
     FORM_PD.USERID.value = '';
@@ -45,6 +44,7 @@ function DummyData(){
     FORM_PD.HEARING_PROBLEM.value = '';
     FORM_PD.KEYBOARD_FAMILIARITY.value = '';
     FORM_PD.PHASENO.value = '';
+	  FORM_PD.GROUPID.value = '';
 	  document.getElementById("Consent").checked = false;
     document.getElementById("ExperimentMode").value = '';
     document.getElementById("NoOfMaps").value = '';
@@ -58,9 +58,8 @@ function DummyData(){
 }
 
 // If Phase 2 or 3 then no need to capture Participant details again
-function disablePD(){
-	FORM_PD = document.ParticipantDetail;
-	if(FORM_PD.PHASENO.value != 'Phase_1'){
+function disablePD(flag){
+	if(flag){
 	  document.getElementById("inputAge").disabled = true;
 		document.getElementById("firstname").disabled = true;
 		document.getElementById("lastname").disabled = true;
@@ -72,6 +71,7 @@ function disablePD(){
 	  document.getElementById("inputMusicKind").disabled = true;
 	  document.getElementById("HearingProblem").disabled = true;
 	  document.getElementById("KeyboardFamiliarity").disabled = true;
+		document.getElementById("GroupId").disabled = true;
 	}
 	else{
     document.getElementById("inputAge").disabled = false
@@ -85,6 +85,7 @@ function disablePD(){
     document.getElementById("inputMusicKind").disabled = false;
     document.getElementById("HearingProblem").disabled = false;
     document.getElementById("KeyboardFamiliarity").disabled = false;
+		document.getElementById("GroupId").disabled = false;
 	}
 }
 
@@ -104,65 +105,60 @@ function participantDetails(){
   HEARING_PROBLEM = FORM_PD.HEARING_PROBLEM.value;
   KEYBOARD_FAMILIARITY = FORM_PD.KEYBOARD_FAMILIARITY.value;
   PHASENO = FORM_PD.PHASENO.value;
-
+	GROUPID = FORM_PD.GROUPID.value;
+	var select = document.getElementById("userid");
 	if(document.getElementById("Consent").checked){
 	  if(USERID != ""){
-	    if(PHASENO == 'Phase_1') {
-	      if (!(isNaN(AGE) || AGE < 1 || EDUCATION == "" || MODEOFCOMM == "" || GENDER == "" || PARTICIPANT_TYPE == ""
-		              || MUSICAL_TRAINING == "" || MUSIC_KIND == "" || HEARING_PROBLEM == "" || KEYBOARD_FAMILIARITY == "")) {
-
-		      TimeStamp = new Date().toString();
-		      if(ParticipantDetails.length==0){
-			      ParticipantDetails.push(['USER_ID','FIRST_NAME','LAST_NAME', 'AGE', 'EDUCATION', 'MODE_OF_COMMUNICATION', 'GENDER', 'PARTICIPANT_TYPE',
-				      'MUSICAL_TRAINING', 'MUSIC_KIND', 'HEARING_PROBLEM', 'KEYBOARD_FAMILIARITY', 'Consent Given','TimeStamp']);
-		      }
-	        ParticipantDetails.push([USERID,FIRSTNAME,LASTNAME, AGE, EDUCATION, MODEOFCOMM, GENDER, PARTICIPANT_TYPE, MUSICAL_TRAINING,
-		                                                            MUSIC_KIND, HEARING_PROBLEM, KEYBOARD_FAMILIARITY, "Yes",TimeStamp]);
-		      $('#contactModal').modal('hide');
-		      if ($(".fa-user").hasClass('detailsAdded') == false) {
-			      $(".fa-user").addClass('detailsAdded');
-		      }
-		      savePD();
-		      //enableExperimentParameters();
-	      }
-		    else{//if details are not correct
-		      alert("Please Enter All and Valid Participant Details");
-	      }
-	    }
-		  else if(PHASENO != ""){//if PHASE NO is other than Phase_1
-		    $('#contactModal').modal('hide');
-		    if ($(".fa-user").hasClass('detailsAdded') == false) {
-			    $(".fa-user").addClass('detailsAdded');
-		    }
-		    //enableExperimentParameters();
-	    }
-		  else{
-		    alert("Please Select a Valid Phase Number");
-	    }
-	  }
+			  if (!(isNaN(AGE) || AGE < 1 || EDUCATION == "" || MODEOFCOMM == "" || GENDER == "" || PARTICIPANT_TYPE == ""
+				  || MUSICAL_TRAINING == "" || MUSIC_KIND == "" || HEARING_PROBLEM == "" || KEYBOARD_FAMILIARITY == "" || GROUPID == "" || PHASENO == "") ) {
+				  TimeStamp = new Date().toString();
+				  if(PD.length==0){
+					  ParticipantDetails.push(['USER_ID','GROUP_ID','FIRST_NAME','LAST_NAME', 'AGE', 'EDUCATION', 'MODE_OF_COMMUNICATION', 'GENDER', 'PARTICIPANT_TYPE',
+						  'MUSICAL_TRAINING', 'MUSIC_KIND', 'HEARING_PROBLEM', 'KEYBOARD_FAMILIARITY','PHASE NUMBER', 'Given Consent','TimeStamp']);
+				  }
+				  if(USERID == "NewParticipant") {
+				    USERID = GROUPID + '_' + GENDER[0] + '_' + FIRSTNAME + '_' + (PD.length + 1).toString(16);
+					  alert('Please Note Your USER_ID for future reference: ' + USERID);
+				  }
+				  else{
+					  USERID = select.options[select.selectedIndex].text;
+					  //alert('Please Note Your USER_ID for future reference: ' + USERID);
+				  }
+			    ParticipantDetails.push([USERID,GROUPID,FIRSTNAME,LASTNAME, AGE, EDUCATION, MODEOFCOMM, GENDER, PARTICIPANT_TYPE, MUSICAL_TRAINING,
+					  MUSIC_KIND, HEARING_PROBLEM, KEYBOARD_FAMILIARITY,PHASENO, "YES",TimeStamp]);
+				  $('#contactModal').modal('hide');
+				  if ($(".fa-user").hasClass('detailsAdded') == false) {
+					  $(".fa-user").addClass('detailsAdded');
+				  }
+				  savePD();
+				  //enableExperimentParameters();
+			  }
+		  else{//if details are not correct
+			  alert("Please Enter All and Valid Participant Details");
+		  }
+		}
 		else{// if USER ID is empty
-		  alert("Please Enter USER ID");
+		  alert("Please Select New Participant or Valid USER ID");
 	  }
 	}
 	else{// if consent is not checked
 		alert("Please read the consent form and give your consent");
 	}
 }
+
 // function to save Participant Details
 function savePD (){
 	var csvRows = [];
 	for(var i=0, l=ParticipantDetails.length; i<l; ++i){
 		csvRows.push(ParticipantDetails[i].join(','));
 	}
-	var csvString = csvRows.join("%0A");
-	var savecsv         = document.createElement('a');
-	savecsv.href        = 'data:attachment/csv,' + csvString;
-	savecsv.target      = '_blank';
-	//savecsv.download    = 'PD_'+FIRSTNAME+'_'+LASTNAME+'_'+USERID +'.csv';
-	savecsv.download    = 'ParticipantDetails.csv';
-	document.body.appendChild(savecsv);
-	savecsv.click();
-	alert("Your results are saved successfully");
+	var csvString = csvRows.join("\n");
+//	var savecsv         = document.createElement('a');
+//	savecsv.href        = 'data:attachment/csv,' + csvString;
+//	savecsv.target      = '_blank';
+//	savecsv.download    = USERID +'.csv';
+//	document.body.appendChild(savecsv);
+//	savecsv.click();
 
 	$.ajax({
 		type: 'POST',
@@ -170,7 +166,7 @@ function savePD (){
 		url: 'http://localhost:3000/writeParticipantData',
 		success: function(data) {
 			console.log('success');
-			alert('Saved Your :' + data);
+			alert(data);
 		}
 	});
 }
@@ -188,12 +184,51 @@ function savePD (){
 //}
 
 function readPD (){
+	PD = [];
+	var select = document.getElementById("userid");
+	var options = [];
 	$.get('http://localhost:3000/readParticipantData', function(PD_Result) {
-		PD_Result = PD_Result.split('\n');
-		ParticipantDetails = [];
-		for(var i= 0; i<PD_Result.length;++i){
-			ParticipantDetails.push([PD_Result[i].split(',')]);
+		if(PD_Result.length != 0)
+		{
+			PD_Result = PD_Result.split('\n');
+			for(var i=0; i<PD_Result.length;++i){
+				PD.push(PD_Result[i].split(','));
+				if(i>0 && PD[i][0]!='' && options.indexOf(PD[i][0])== -1 ){// Select Unique User ID's
+					var el = document.createElement("option");
+					options.push(PD[i][0]);
+					el.text = PD[i][0];
+					el.value = i;
+					select.appendChild(el);
+				}
+			}
 		}
-		alert('read:' +ParticipantDetails.length);
+		alert('No of Rows in PD:' +PD.length);
 	});
+}
+
+function populatePD(){
+	FORM_PD = document.ParticipantDetail;
+	if(FORM_PD.USERID.value != 'NewParticipant'){
+		var i = FORM_PD.USERID.value;
+		FORM_PD.GROUPID.value = PD[i][1];
+		FORM_PD.FIRSTNAME.value = PD[i][2];
+		FORM_PD.LASTNAME.value = PD[i][3];
+		FORM_PD.AGE.value = PD[i][4];
+		FORM_PD.EDUCATION.value = PD[i][5];
+		FORM_PD.MODEOFCOMM.value = PD[i][6];
+		FORM_PD.GENDER.value = PD[i][7];
+		FORM_PD.PARTICIPANT_TYPE.value = PD[i][8];
+		FORM_PD.MUSICAL_TRAINING.value = PD[i][9];
+		MUSIC_KIND = FORM_PD.MUSIC_KIND.value = PD[i][10];
+		FORM_PD.HEARING_PROBLEM.value = PD[i][11];
+		FORM_PD.KEYBOARD_FAMILIARITY.value = PD[i][12];
+		FORM_PD.PHASENO.value = '';
+		document.getElementById("PhaseNo").disabled = false;
+		disablePD(true);
+	}
+	else{
+		FORM_PD.PHASENO.value = 'Phase_1';
+		document.getElementById("PhaseNo").disabled = true;
+		disablePD(false);
+	}
 }
--- server.js
+++ server.js
@@ -7,8 +7,44 @@ var http = require("http"),
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+/* serve ViShruti page at Root*/
 app.get('/', function (req, res) {
 	res.sendfile(__dirname+'/public/'+'ViShruti.html');
+});
+
+/* Read Participant data */
+app.get('/readParticipantData',function(req,res){
+	console.log('In readPD');
+	console.log('read file :'+ __dirname+'/ParticipantData/ParticipantDetails.csv');
+	fs.readFile(__dirname+'/ParticipantData/ParticipantDetails.csv', 'utf8', function (err, data) {
+		var msg = [];
+		if (err){
+			console.log('Error Occurred '+err);
+			res.end(msg);
+		}else{
+			console.log('Data Read:\n'+ data);
+			res.end(data);
+		}
+	});
+});
+
+/* Writ Participant Data */
+app.post('/writeParticipantData',function(req,res){
+	var data = '';
+	req.on('data', function(chunk) {
+		console.log("Received body data:");
+		data += chunk.toString();
+		console.log(chunk.toString());
+	});
+
+	req.on('end', function() {
+		console.log(__dirname+'/ParticipantData/ParticipantDetails.csv');
+		fs.appendFile(__dirname+'/ParticipantData/ParticipantDetails.csv', data+'\n', function (err) {
+			if (err) throw err;
+			console.log('The data is saved into file');
+		});
+		res.end('The data is saved into file');
+	});
 });
 
 
@@ -19,46 +55,12 @@ app.post('/saveExperimentData',function(req,res){
 	});
 });
 
-
-app.get('/readParticipantData',function(req,res){
-	console.log('read file :'+ __dirname+'/ParticipantData/ParticipantDetails.csv');
-	fs.readFile(__dirname+'/ParticipantData/ParticipantDetails.csv', 'utf8', function (err, data) {
-		if (err){
-			if(err.errno==34){
-				res.end('No such file exist , error:' + err);
-			}
-		}else{
-			console.log('read: '+ data);
-			res.end(data);
-		}
-	});
-});
-
-	app.post('/writeParticipantData',function(req,res){
-		var data = '';
-		req.on('data', function(chunk) {
-			console.log("Received body data:");
-			data += chunk;
-			console.log(chunk.toString());
-		});
-
-		req.on('end', function() {
-			console.log(__dirname+'/ParticipantData/ParticipantDetails.csv');
-			fs.appendFile(__dirname+'/ParticipantData/ParticipantDetails.csv', data, function (err) {
-				if (err) throw err;
-				console.log('The data is saved into file');
-			});
-			res.end();
-		});
-	});
-
 /* serves all the static files */
 app.get(/^(.+)$/, function(req, res){
 	console.log('static file request : ' + req.params[0]);
 	res.sendfile( __dirname + '/public/'+req.params[0]);
 
 });
-
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));

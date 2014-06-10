de21c40c58aa51c44032c3708a0c1ee8fcc319c0
sanchitaggarwal <sanchitagarwal108@gmail.com>
2014-06-10 08:17:07 +0530

    Fixed Time and Server Sync

--- public/js/inputcontrols.js
+++ public/js/inputcontrols.js
@@ -65,7 +65,7 @@ var Axes = {
 var ANALOGUE_BUTTON_THRESHOLD = .5;
 var AXIS_DEADZONE = 0.9;
 var Axes_X,Axes_Y,oldAxes_X,oldAxes_Y,Theta,oldTheta,quad,oldquad;
-document.onkeyup = onUserInput;
+var KeyBoard = false;
 
 buttonPressed = function(stick, buttonId) {
 	//console.log(stick.buttons[buttonId]);
@@ -85,6 +85,7 @@ function initJoyStick(){
 		joyStickDetected = true;
 	}
 }
+document.onkeyup = KeyBoardResponse;
 
 function findIndex(search_array,key){
 	var index = -1;
@@ -132,7 +133,9 @@ function onUserInput() {
 		initJoyStick();
 	}
 	else{
-		KeyBoardResponse();
+		//console.log('KeyBoard Control');
+		KeyBoard = true;
+
 	}
 
 }
@@ -187,9 +190,18 @@ function playNextTrial(){
 		}
 		TimeStamp = new Date().toString();
 		ExperimentData = [];
-		ExperimentData.push(USERID,GROUPID,PHASENO,CurrentMode,TrialNo,CurrentMapNo,TrialLength,Direction,InterStimulusInterval);
+		ExperimentData.push(USERID,GROUPID,PHASENO,CurrentMode,Direction,FileIndex,CurrentMapNo,TrialNo,TrialLength,InterStimulusInterval);
 		ExperimentData.push(TrialLabels.join(','),ResponseLabels.join(','),TrialTime,ResponseTime.join(','),TotalResponseTime,Hit,Miss,Recall,TrialLength*Recall,ExtraResponse.join(' '),TimeStamp);
 		save(ExperimentData_Filename,ExperimentData);
+		TrialLabels = [];
+		ResponseLabels = [];
+		ResponseTime = [];
+		ExtraResponse = [' '];
+		AverageResponseTime = ((next-1)*AverageResponseTime + TotalResponseTime)/next;
+		TotalResponseTime = 0;
+		Hit = 0;
+		Miss = 0;
+		Recall = 0;
 	}
 	count=0;
 	next = CurrentCuePos;
@@ -233,14 +245,17 @@ function checkResponse(response_key){
 	if(response_key == cue_index){
 		count++;
 		Hit++;
+		TotalHit++;
 		if(VisualError && noextra){Maze[x][y] = 4 * (TrialNo % 2) + 3;}
 	}
 	else{
 		if(AudioError){playError();}
 		Miss++;
+		TotalMiss++;
 		count--;
 		if(VisualError && noextra){Maze[x][y] = 4 * (TrialNo % 2) + 4;}
 	}
+	currentAccuracy = 100*TotalHit/(TotalStimuli);
 	drawMaze(Maze,MazeLength);
 	drawMetrics();
 	drawControls(response_key);
@@ -248,21 +263,26 @@ function checkResponse(response_key){
 }
 
 function isCompleteResponse(){
-	while(next<CurrentCuePos){
-		var x =  Path[next][0];
-		var y = Path[next][1];
-		var cue_x = Cue[next][0];
-		var cue_y = Cue[next][1]*-1;
-		var cue_code = 10 * (cue_x + 2) + (cue_y + 2);
-		var cue_index = findIndex(expectedDirection,cue_code);
-		TrialLabels.push(DirectionLabels[cue_index]);
-		ResponseLabels.push('NoInput');
-		if(VisualError){Maze[x][y] = 4 * (TrialNo % 2) + 4;}
-		Miss++;
-		count--;
-		next++;
+	var TRTime = 0;
+	if(next<CurrentCuePos){
+		while(next<CurrentCuePos){
+			var x =  Path[next][0];
+			var y = Path[next][1];
+			var cue_x = Cue[next][0];
+			var cue_y = Cue[next][1]*-1;
+			var cue_code = 10 * (cue_x + 2) + (cue_y + 2);
+			var cue_index = findIndex(expectedDirection,cue_code);
+			TrialLabels.push(DirectionLabels[cue_index]);
+			ResponseLabels.push('NoInput');
+			TRTime = TotalResponseTime + InterResponseTime;
+			if(VisualError){Maze[x][y] = 4 * (TrialNo % 2) + 4;}
+			Miss++;
+			count--;
+			next++;
+		}
+		TotalResponseTime = TRTime;
 	}
-	for(var i=TrialLabels.length;i<=WM_SETSIZE;i++){
+	for(var i=TrialLabels.length;i<WM_SETSIZE;i++){
 		TrialLabels.push('empty');
 		ResponseLabels.push('empty');
 		ResponseTime.push('0.0');
@@ -287,13 +307,14 @@ function isCompleteMap(){
 		SaveCanvasImage();
 		isConsecutiveMapAccuracy();
 		next = 0;
-		Hit = 0;
-		Miss = 0;
-		ResponseTime = 0;
+		Hit = 0; TotalHit = 0;
+		Miss = 0; TotalMiss = 0;
+		ResponseTime = []; TotalResponseTime = 0;
 		CurrentCuePos = 0;
-		count=0;Recall=0;
+		count=0; TotalRecall=0; Recall = 0;
 		TrialNo = 0;
 		PopNextFunction = 1;
+		AverageResponseTime = 0;
 		return true;
 	}
 	else{
@@ -357,7 +378,7 @@ else{
 function SaveCanvasImage(){
 	var canvas = document.getElementById('Maze_Canvas');
 	var imageURL = canvas.toDataURL('image/png');
-	var filename = User_DataFolder + USERID + CurrentMode + "_Dir_" + Direction + "_Map_" + CurrentMapNo + "_FI_" + FileIndex + ".png";
+	var filename = User_DataFolder + USERID + '_' + PHASENO + '_' + CurrentMode + "_Dir_" + Direction + "_Map_" + CurrentMapNo + "_FI_" + FileIndex + ".png";
 	$.ajax({
 		type: 'POST',
 		data: {'Name': filename,
@@ -375,7 +396,6 @@ function SaveCanvasImage(){
 }
 
 function isConsecutiveMapAccuracy(){
-	currentAccuracy = 100*Hit/(TotalStimuli);
 	consecutiveMapAccuracy = (consecutiveMapAccuracy + currentAccuracy)/ConsecutiveMap;
 	console.log('Consecutive Map Accuracy: ' + consecutiveMapAccuracy);
 	var EM = ExperimentList[CurrentMode];
@@ -414,180 +434,30 @@ function isConsecutiveMapAccuracy(){
 }
 //globalID = requestAnimationFrame(joyStickResponse);
 function KeyBoardResponse(){
-	var waitTime = new Date().getTime();
-	if(!Sounds[counter] && counter>0){
-		StartExp = true;
-	}
-	if(StartExp && !ExperimentEnd){
-		//var key_press = String.fromCharCode(event.keyCode);
+	if(StartExp && !ExperimentEnd && KeyBoard){
 		var key_code = event.keyCode;
 		var key_index_a = findIndex(inputDirection_a,key_code);
 		var key_index_b = findIndex(inputDirection_b,key_code);
-		if(key_index_a == -1 && key_index_b == -1){
+		var input_index = key_index_a > key_index_b ? key_index_a:key_index_b;
+		if(key_index_a == 9 ||key_index_b==9){
+			console.log("Key Pressed, Play Next Trial");
+			playNextTrial();
+		}
+		else if(key_index_a == -1 && key_index_b == -1){
 			console.log('Invalid Input');
 		}
 		else{
-			if(key_index_a == 9 ||key_index_b==9){
-				console.log('Spacebar: next '+next+' CurrentCuePos '+CurrentCuePos+" TotalStimuli:"+TotalStimuli);
-				while(next<CurrentCuePos){
-					var x =  Path[next][0];
-					var y = Path[next][1];
-					var cue_x = Cue[next][0];
-					var cue_y = Cue[next][1]*-1;
-					var cue_code = 10 * (cue_x + 2) + (cue_y + 2);
-					var cue_index = findIndex(expectedDirection,cue_code);
-					TrialLabels.push(DirectionLabels[cue_index]);
-					ResponseLabels.push('NoInput');
-					if(VisualError){Maze[x][y] = 4 * (TrialNo % 2) + 4;}
-					Miss++;
-					count--;
-					next++;
-				}
-				// Add marker in input and cue labels
-				TrialLabels.push(DirectionLabels[key_index_a]);
-				ResponseLabels.push(DirectionLabels[key_index_a]);
-				if(count==TrialLength){Recall++;}
-				ResponseTime.push(IntervalTime);
-				ResponseTime = ResponseTime + IntervalTime;
-				IntervalTime = 0;
-				count=0;
-				next = CurrentCuePos;
-				if(CurrentCuePos==TotalStimuli && PopNextFunction == 0){ // pop next function for new Maps
-					if(Staircase){
-						FamiliarRecall = Recall;
-						FamiliarISI = FamiliarRecall>=maxRecall?ISIList[ISICounter-1]:FamiliarISI;
-						maxRecall =  FamiliarRecall>=maxRecall?FamiliarRecall:maxRecall;
-					}
-					drawMaze(Maze,MazeLength);
-					drawMetrics();
-					if(CurrentMode == 'InCorrectVisualCue'){
-						drawCorrectMaze(MazeLength,Path);
-					}
-					var canvas = document.getElementById('Maze_Canvas');
-					var savecanvas = document.createElement('a');
-					savecanvas.href = canvas.toDataURL('image/png').replace('image/png');
-					savecanvas.download= USERID + "_" + CurrentMode + "_Dir_" + Direction + "_Map_" + CurrentMapNo +"_PL_" + TotalStimuli + "_FI_" + FileIndex + ".png";
-					document.body.appendChild(savecanvas);
-					savecanvas.click();
-
-					if(WM){
-						TrialLength = WM_CueLength.toString();
-					}
-					if(Staircase){
-						TrialLength = StaircaseCueLength.toString();
-					}
-					var currentAccuracy = 100*Hit/(TotalStimuli);
-					AvgAccuracy = ((CurrentMapNo - 1)*AvgAccuracy + currentAccuracy)/CurrentMapNo;
-					console.log(AvgAccuracy);
-					ExperimentResults.push([FileIndex,Direction,TrialLength,CurrentMode,AccuracyThreshold,TotalStimuli,Hit,Miss,100*Hit/(TotalStimuli),Recall,ResponseTime,ResponseTime/TotalStimuli,NoOfMaps,ResponseTime.toString(),TrialLabels.toString(),ResponseLabels.toString(),InterStimulusInterval,AvgAccuracy]);
-					var KeyExp = ExperimentList[CurrentMode];
-					console.log('Accuracy Flag :'+AccuracyFlag);
-					if(CurrentMapNo>5 && KeyExp < 5){
-						if(currentAccuracy >= AccuracyThreshold){
-							AccuracyFlag = AccuracyFlag -1;
-						}
-						else{
-							AccuracyFlag = 3;
-						}
-					}
-					else{
-						AccuracyFlag = 3;
-					}
-					if(AccuracyFlag <= 0){
-						console.log("Accuracy for three consecutive map is greater than threshold ,switching to another mode");
-						var i = CurrentMapNo;
-						while(i<NMaps){
-							FQCounter--;
-							FileIndex++;
-							FunctionQueue.shift();
-							console.log("Spliced the function call");
-							i++;
-						}
-					}
-					else{
-						if(CurrentMapNo==NoOfMaps && KeyExp < 5){
-							ExperimentEnd = 1;
-							// clear the polling variable
-							alert('Avg Accuracy of three consecutive Maps less than Accuracy Threshold '+ AccuracyThreshold +' After '+NMaps + ' Maps.\nTerminating Experiment');
-							while(i<NMaps){
-								FQCounter--;
-								FileIndex++;
-								FunctionQueue.shift();
-								console.log("Spliced the function call");
-								i++;
-							}
-							clearInterval(checkFunctionQueue);
-							stopExperiment();
-						}
-					}
-
-					next = 0;
-					Hit = 0;
-					Miss = 0;
-					ResponseTime = 0;
-					CurrentCuePos = 0;
-					count=0;Recall=0;
-					TrialNo = 0;
-					PopNextFunction = 1;
-				}
-				else{ //play next cues
-					// inter-trial time between two sound patterns in working memory experiment
-					startexp = false;
-					var str1 = "./Silence/silence";
-					silencefile = str1.concat(InterTrialInterval,'.wav');
-					AddSilence();
-					// play next audio sequence
-					NextCue();
-					playSounds();
-					TrialTime =  new Date().getTime();
-				}
-				drawMaze(Maze,MazeLength);
-				drawMetrics();
-				drawControls(key_index_a);
-			}
-			else{
-				var x, y,cue_x,cue_y,cue_index;
-				var noextra = true;
-				if(next < CurrentCuePos){
-					x =  Path[next][0];
-					y = Path[next][1];
-					cue_x = Cue[next][0];
-					cue_y = Cue[next][1]*-1;
-					cue_code = 10 * (cue_x + 2) + (cue_y + 2);
-					cue_index = findIndex(expectedDirection,cue_code);
-					TrialLabels.push(DirectionLabels[cue_index]);
-				}
-				else{
-					noextra = false;
-					cue_index = -2;
-					TrialLabels.push('NoCues');
-				}
-				var input_index = key_index_a > key_index_b ? key_index_a:key_index_b;
-				ResponseLabels.push(DirectionLabels[input_index]);
-				if(key_index_a == cue_index || key_index_b == cue_index){
-					IntervalTime = IntervalTime + (waitTime - TrialTime )/1000;
-					TrialTime = new Date().getTime();
-					count++;
-					Hit++;
-					if(VisualError && noextra){Maze[x][y] = 4 * (TrialNo % 2) + 3;}
-				}
-				else{
-					IntervalTime = IntervalTime + (waitTime - TrialTime)/1000;
-					TrialTime = new Date().getTime();
-					if(AudioError){playError();}
-					Miss++;
-					count--;
-					if(VisualError && noextra){Maze[x][y] = 4 * (TrialNo % 2) + 4;}
-				}
-				drawMaze(Maze,MazeLength);
-				drawMetrics();
-				drawControls(key_index_a>key_index_b?key_index_a:key_index_b);
-				next++;
-			}
+			InterResponseTime = (new Date().getTime() - waitTime)/1000;
+			waitTime = new Date().getTime();
+			console.log(input_index);
+			console.log("Response Recorded: ");
+			checkResponse(input_index);
 		}
 	}
+	else{
+		console.log('No Input');
+	}
 }
-
 
 //$("#StartExp").on("click", function() {
 //});
@@ -596,7 +466,6 @@ function KeyBoardResponse(){
 //	if(USERCONTROL == 'JoyStick')
 //		cancelAnimationFrame(globalID);
 //});
-
 
 //function joyStickResponse(){
 //	if (!joyStickDetected) {
